7b26a2a98ae9baad6bb2bbb0116d26b6e8dceed8
RossDooney <rossdooney@gmail.com>
2023-12-04 11:39:30 +0000

    http client added

--- server/main.go
+++ server/main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in env")
+	}
+	fmt.Printf("Port: %v \n", portString)
 
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+
+	router.Mount("/", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
+	fmt.Printf("Server starting on port: %v \n", portString)
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
